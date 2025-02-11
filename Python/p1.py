6e71a2a61dfd2f26e474f7d8d75f14a052b6fff4
Saad Monem <smonem@umich.edu>
2025-02-11 10:53:26 -0500

    Added first Python assignment

---
+++ Python/p1.py
@@ -0,0 +1,14 @@
+# Eligible Elector
+
+age = input("How old are you? ")
+
+# Error Checking
+if not str.isnumeric(age) or int(age) < 1:
+    print("Error: invalid input.")
+    exit(0)
+
+age = int(age)
+if age < 18:
+    print(f"Oops! You’re not eligible yet. But hey, only {18 - age} more year{"s" if age != 17 else ""} to go!")
+else:
+    print("Congratulations! You are eligible to vote. Go make a difference!")
